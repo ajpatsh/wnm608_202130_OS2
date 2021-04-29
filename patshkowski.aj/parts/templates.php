f85ab577ea6359060bdbc6891f1f1ca6545e0e11
Amanda Patshkowski <amandapatshkowski@Amandas-MBP.attlocal.net>
2021-04-29 13:31:56 -0700

    mod homework

--- patshkowski.aj/parts/templates.php
+++ patshkowski.aj/parts/templates.php
@@ -5,13 +5,30 @@ return $r.<<<HTML
 <div class="col-xs-12 col-sm-6 col-md-4">
    <a href="product_item.php?id=$o->id" class="product">
       <div class="product-image">
-         <img src="$o->image_thumb" alt=""> /* MAKE SURE TO ADD CORRECT IMAGE LOCATION */
+         <img src="img/items/$o->image_thumb" alt=""> 
       </div>
       <figcaption class="product-caption">
          <div class="product-price">&dollar;$o->price</div>
          <div class="product-title">$o->name</div>
       </figcaption>
    </a>
+</div>
+HTML;
+}
+
+function makeCartList($r,$o) {
+return $r.<<<HTML
+<div class="display-flex">
+   <div class="flex-none image-thumbs">
+      <img src="$o->image_thumb">
+   </div>
+   <div class="flex-stretch">
+      <strong>$o->title</strong>
+      <div>Delete</div>
+   </div>
+   <div class="flex-none">
+      &dollar;$o->price
+   </div>
 </div>
 HTML;
 }
--- patshkowski.aj/product_item.php
+++ patshkowski.aj/product_item.php
@@ -2,25 +2,72 @@
 
 include "lib/php/functions.php";
 
+$product = MYSQLIQuery("
+   SELECT *
+   FROM `products`
+   WHERE `id` = {$_GET['id']}
+")[0];
+
+$thumbs = explode(",", $product->image_other);
+
+$thumb_elements = array_reduce($thumbs,function($r,$o){
+   return $r."<img src='img/items/$o'>";
+});
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	
-	<title>Product Item</title>
-
-	<?php include "parts/meta.php" ?>
-	
+   <title>Product List</title>
+   
+   <?php include "parts/meta.php" ?>
 </head>
 <body>
-	
-	<?php include "parts/navbar.php" ?>
+   <?php include "parts/navbar.php" ?>
+   
 
-	<div class="container">
-		<div class="card soft">
-			<h2>Product Item</h2>
-
-			<div>This is product item: #<?= $_GET['id'] ?></div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="grid gap product-display">
+         <div class="col-xs-12 col-md-7">
+            <div class="card soft">
+               <div class="image-main">
+                  <img src="img/items/<?= $product->image_main ?>" />
+               </div>
+               <div class="image-thumbs"><?= $thumb_elements ?></div>
+            </div>
+         </div>
+         <div class="col-xs-12 col-md-5">
+            <div class="card soft flat">
+               <div class="card-section">
+                  <div class="product-title"><?= $product->name ?></div>
+                  <div class="product-price">&dollar;<?= $product->price ?></div>
+               </div>
+               <div class="card-section">
+                  <label class="form-label">Amount</label>
+                  <div class="form-select">
+                     <select>
+                        <!-- option[value='$']*10>{$} -->
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                     </select>
+                  </div>
+               </div>
+               <div class="card-section">
+                  <a href="product_added_to_cart.php" class="form-button sell">Add To Cart</a>
+               </div>
+            </div>
+            <div class="card soft">
+               <?= $product->description ?>
+            </div>
+         </div>
+      </div>
+   </div>
 </body>
 </html>
